0811f7f5414ad1bf2934000d5ea520f43fb67114
Vesa Mattila <vesa.m.mattila@outlook.com>
2020-09-19 11:27:46 +0300

    Added tesets for days_needed function

--- unittests.py
+++ unittests.py
@@ -1,12 +1,13 @@
 import unittest
-from holidayplanner import HolidayPlanner
+from holidayplanner import HolidayPlanner,HolidayStartLaterThanEnd,HolidayRangeTooWide
+from datetime import date, timedelta, datetime
 
 
 class TestHolidayPlanner(unittest.TestCase):
 
     def test_planner_default_init(self):
         p = HolidayPlanner()
-        assert(p)
+        self.assertTrue(p)
         
     def test_planner_with_file_param(self):
         with self.assertRaises(FileNotFoundError):    
@@ -19,6 +20,22 @@ class TestHolidayPlanner(unittest.TestCase):
     def test_planner_with_non_string(self):
         with self.assertRaises(ValueError):    
             HolidayPlanner(123)
+            
+    def test_days_needed_start_later_than_end(self):
+        planner = HolidayPlanner()
+        with self.assertRaises(HolidayStartLaterThanEnd):
+            planner.days_needed(date(2020,12,31),date(2020,10,1))
+            
+    def test_days_needed_range_over_max_range(self):
+        planner = HolidayPlanner()
+        with self.assertRaises(HolidayRangeTooWide):
+            planner.days_needed(date(2020,10,1),date(2020,12,1))
+    
+    def test_days_needed_valid_month(self):
+        planner = HolidayPlanner()
+        planner.days_needed(date(2020,10,1),date(2020,10,31))
+        self.assertEqual(planner.days_needed(date(2020,10,1),date(2020,10,31)),26)
+
     
         
 
