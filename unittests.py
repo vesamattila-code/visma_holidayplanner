1ef93eae215fadd1dd608a6453034a7f0db68e9d
Vesa Mattila <vesa.m.mattila@outlook.com>
2020-09-19 10:39:21 +0300

    added unittest and date file. first unittests added

--- unittests.py
+++ unittests.py
@@ -1,1 +1,28 @@
+import unittest
+from holidayplanner import HolidayPlanner
 
+
+class TestHolidayPlanner(unittest.TestCase):
+
+    def test_planner_default_init(self):
+        p = HolidayPlanner()
+        assert(p)
+        
+    def test_planner_with_file_param(self):
+        with self.assertRaises(FileNotFoundError):    
+            HolidayPlanner('non_existing_file.cvs')
+        
+    def test_planner_with_empty_param(self):
+        with self.assertRaises(FileNotFoundError):    
+            HolidayPlanner('non_existing_file')
+        pass
+        
+    def test_planner_with_non_string(self):
+        with self.assertRaises(ValueError):    
+            HolidayPlanner(123)
+        pass
+    
+        
+
+if __name__ == '__main__':
+    unittest.main()
